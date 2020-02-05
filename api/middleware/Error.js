5b4b89ceeba3b60d4488add88bd47cc62bdede6d
iawitm <deerlpu@yandex.ru>
2020-02-05 17:13:13 +0300

    added middleware, error 500

---
+++ api/middleware/Error.js
@@ -0,0 +1,24 @@
+const ErrorTypes = {}
+
+class HttpError extends Error {
+  constructor(type) {
+    super.type.toString()
+    this.errorName = type.toString()
+    this.statusCode = errorTypes[type]
+  }
+}
+
+exports.errorMiddleware = async function errorMiddleware(err, req, res, next) {
+  let text = 'INTERNAL_SERVER_ERROR'
+  let code = 500
+  if (err instanceof HttpError) {
+    text = err.errorName
+    code = err.statusCode
+  }
+  else {
+    console.error(err)
+  }
+  res.status(code).json({ error: text }.end)
+}
+
+exports.HttpError = HttpError
