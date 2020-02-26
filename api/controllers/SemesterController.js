b36f9be7422f8d317d4c480b741df64a9c35171b
iawitm <deerlpu@yandex.ru>
2020-02-26 03:27:13 +0300

    MA-59: Added get semester method

--- api/controllers/SemesterController.js
+++ api/controllers/SemesterController.js
@@ -2,17 +2,25 @@ const Semester = require('../model/SemesterModel')
 const HttpError = require('../middleware/Error').HttpError
 
 exports.setSemester = async (req, res, next) => {
-  const { startDate, endDate } = req.body
+    const { startDate, endDate } = req.body
 
-  const startCalendar = Date.parse(startDate)
-  const endCalendar = Date.parse(endDate)
+    const startCalendar = Date.parse(startDate)
+    const endCalendar = Date.parse(endDate)
 
-  if (startCalendar >= endCalendar) throw new HttpError('INCORRECT_DATE')
+    if (startCalendar >= endCalendar) throw new HttpError('INCORRECT_DATE')
 
-  const filter = {}
-  const update = { startDate: startCalendar, endDate: endCalendar }
+    const filter = {}
+    const update = { startDate: startCalendar, endDate: endCalendar }
 
-  let doc = await Semester.findOneAndUpdate(filter, update, { new: true, upsert: true })
+    let doc = await Semester.findOneAndUpdate(filter, update, { new: true, upsert: true })
 
-  res.status(200).json({created: doc})
+    res.status(200).json({ created: doc })
+}
+
+exports.getSemester = async (req, res, next) => {
+    let semester = await Semester
+        .findOne()
+        .select('-_id')
+
+    res.status(200).json(semester)
 }
