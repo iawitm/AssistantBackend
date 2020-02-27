c0b667a28a81091f2fec75859c6b63ddcef9ec45
iawitm <deerlpu@yandex.ru>
2020-02-27 16:34:42 +0300

    MA-88: protected post exam route with admin middleware

--- api/routes/exams/ExamRoute.js
+++ api/routes/exams/ExamRoute.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router()
+const checkAdmin = require('../../middleware/Admin')
 
 const ExamController = require('../../controllers/exams/ExamController')
 
-router.post('/', ExamController.uploadExams)
+router.post('/', checkAdmin, ExamController.uploadExams)
 router.get('/', ExamController.getExams)
 
 module.exports = router
